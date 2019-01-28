9d2ed1b115adbff703ac5372f5cb330e5dfb7c4b
Anurag Singh <anursingh28@gmail.com>
2019-01-28 21:40:11 +0530

    Contact Form submit

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Contact
+# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_dispaly = ('id','name','listing','email','contact_date')
+    list_dispaly_link = ('id','name')
+    search_fields = ('name','email','listing')
+    list_per_page = 25
+
+
+
+admin.site.register(Contact,ContactAdmin)
+
